f4f671069fff98bcff209dfaebf85d1f2d404950
Komron-Mirzo <kabduvaliyev@gmail.com>
2023-04-09 17:16:45 +0500

    Tale section is done

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -10,8 +10,8 @@ function Table() {
         <div className="table-container" >
             <div className="table-content">
                 <SmallTitle title="BOOKING TABLE FOR YOUR & FAMILY MEMBERS" fontSz="14px" justifyContent="center" marginDiv="0 0 15px 0" />
-                <BigTitle title="Need a Table On Coffee House" fontSz="40px" margin="0px 0px 57px 0px" textAlign="center" />
-                <Button buttonTitle="Booking Table" margin="15px 47px" />
+                <BigTitle title="Need a Table On Coffee House" fontSz="40px" margin="0px 0px 20px 0px" textAlign="center" />
+                <Button buttonTitle="Booking Table" padding="15px 47px" border="solid 1px #fff" fontSz={16} margin="auto" />
 
             </div>
         </div>
