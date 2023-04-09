61d68de48efcfccdca443ae2910831529f557e94
Komron-Mirzo <kabduvaliyev@gmail.com>
2023-04-09 22:12:29 +0500

    Footer is done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import './Footer.css';
 import Logo from '../NavBar/logo-coffee.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagramSquare, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
 
@@ -16,17 +17,70 @@ function Footer() {
                         <div className="social-media">
                             <span>
                                 <a href="#" >
-                                    <FontAwesomeIcon icon="fa-brands fa-facebook" />
+                                    <FontAwesomeIcon icon={faFacebook} />
+                                </a>
+                            </span>
+                            <span>
+                                <a href="#" >
+                                    <FontAwesomeIcon icon={faInstagramSquare} />
+                                </a>
+                            </span>
+                            <span>
+                                <a href="#" >
+                                    <FontAwesomeIcon icon={faTwitter} />
+                                </a>
+                            </span>
+                            <span>
+                                <a href="#" >
+                                    <FontAwesomeIcon icon={faLinkedin} />
                                 </a>
                             </span>
                         </div>
 
                     </div>
                     <div className="f-up-two">
+                        <h2 className='footer-title'>
+                            Working Hours
+                        </h2>
+                        <ul className='footer-list'>
+                            <li>
+                                <span className='footer-brown-text'>Sunday - Thursday</span><br />
+                                <span className='footer-white-text'>08:00 am - 09:00pm</span>
+                            </li>
+                            <li>
+                                <span className='footer-brown-text'>Only Friday
+                                </span><br />
+                                <span className='footer-white-text'>03:00 pm - 09:00pm</span>
+                            </li>
+                            <li>
+                                <span className='footer-brown-text'>Saturday</span><br />
+                                <span className='footer-white-text'>Closed</span>
+                            </li>
+                        </ul>
 
                     </div>
                     <div className="f-up-three">
+                        <h2 className='footer-title'>
+                            Contact Us
+                        </h2>
+                        <ul className='footer-list'>
+                            <li>
+                                <span className='footer-brown-text'>Location:</span><br />
+                                <span className='footer-white-text'>Tashkent, Uzbekistan
+                                </span>
+                            </li>
+                            <li>
+                                <span className='footer-brown-text'>Email Address :
 
+                                </span><br />
+                                <span className='footer-white-text'>kabduvaliyev@gmail.com</span>
+                            </li>
+                            <li>
+                                <span className='footer-brown-text'>Phone Number :
+                                </span><br />
+                                <span className='footer-white-text'>+998 (90) 984 09 94</span>
+                            </li>
+                        </ul>
                     </div>
 
                 </div>
